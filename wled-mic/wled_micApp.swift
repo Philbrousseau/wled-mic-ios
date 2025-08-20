a1b977742623e7cf60f83255daf37dbea5edfd36
Alex Volkov <altryne@gmail.com>
2025-08-20 08:46:10 -0600

    Initial Commit

---
+++ wled-mic/wled_micApp.swift
@@ -0,0 +1,17 @@
+//
+//  wled_micApp.swift
+//  wled-mic
+//
+//  Created by Alex Volkov on 8/20/25.
+//
+
+import SwiftUI
+
+@main
+struct wled_micApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ wled-micTests/wled_micTests.swift
@@ -0,0 +1,17 @@
+//
+//  wled_micTests.swift
+//  wled-micTests
+//
+//  Created by Alex Volkov on 8/20/25.
+//
+
+import Testing
+@testable import wled_mic
+
+struct wled_micTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
